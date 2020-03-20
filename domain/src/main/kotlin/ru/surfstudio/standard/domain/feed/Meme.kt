3ed7cbbfc421adc51f352f3bc649f1f907f63fc9
Vlad <vladder2312@mail.ru>
2020-03-20 16:45:07 +0300

    Выводится дата создания

--- domain/src/main/kotlin/ru/surfstudio/standard/domain/feed/Meme.kt
+++ domain/src/main/kotlin/ru/surfstudio/standard/domain/feed/Meme.kt
@@ -2,7 +2,7 @@ package ru.surfstudio.standard.domain.feed
 
 data class Meme(
         val id: String,
-        val createdDate: Int,
+        val createdDate: Long,
         val description: String,
         val isFavorite: Boolean,
         val photoUtl: String,
--- f-meme/src/main/java/com/example/f_meme/MemeActivityView.kt
+++ f-meme/src/main/java/com/example/f_meme/MemeActivityView.kt
@@ -36,10 +36,10 @@ class MemeActivityView : BaseRxActivityView() {
     private fun loadMeme(){
         titleFullMeme.text = intent.getStringExtra("title")
         descriptionFullMeme.text = intent.getStringExtra("description")
+        dateFullMeme.text = getTimeAgo(intent.getLongExtra("createdDate",0))
         ImageLoader.with(applicationContext)
                 .url(intent.getStringExtra("imageUtl"))
                 .into(imageFullMeme)
-        dateFullMeme.text = intent.getStringExtra("createdDate")
     }
 
     private fun bind(){
@@ -55,6 +55,34 @@ class MemeActivityView : BaseRxActivityView() {
             like_meme_btn.setImageResource(R.drawable.icon_like_filled)
         } else {
             like_meme_btn.setImageResource(R.drawable.icon_like)
+        }
+    }
+
+    private fun getTimeAgo(time: Long): String? {
+        val SECOND_MILLIS = 1000
+        val MINUTE_MILLIS = 60 * SECOND_MILLIS
+        val HOUR_MILLIS = 60 * MINUTE_MILLIS
+        val DAY_MILLIS = 24 * HOUR_MILLIS
+
+        var time = time
+        if (time < 1000000000000L) {
+            time *= 1000
+        }
+
+        val now = System.currentTimeMillis()
+        if (time > now || time <= 0) {
+            return null
+        }
+
+        val diff = now - time
+        return when {
+            diff < MINUTE_MILLIS -> " только что"
+            diff < 2 * MINUTE_MILLIS -> " минуты назад"
+            diff < 50 * MINUTE_MILLIS -> (diff / MINUTE_MILLIS).toString() + " минут назад"
+            diff < 90 * MINUTE_MILLIS -> " час назад"
+            diff < 24 * HOUR_MILLIS -> (diff / HOUR_MILLIS).toString() + " часов назад"
+            diff < 48 * HOUR_MILLIS -> "вчера"
+            else -> (diff / DAY_MILLIS).toString() + " дней назад"
         }
     }
 }
